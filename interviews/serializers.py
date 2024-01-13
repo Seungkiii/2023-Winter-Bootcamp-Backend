ecaccd588b17b8ff9c0f5e45cd6e80e8057256bd
nagosu <nozick1021@gmail.com>
2024-01-14 04:45:46 +0900

    feat : 답변 등록 Whisper API 및 GPT 후처리 구현 완료

--- interviews/serializers.py
+++ interviews/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Interview_Type, Question, Interview, Type_Choice, Answer, Repository
-from .utils import handle_uploaded_file_s3
 
 # 면접 결과 조회 Serializer
 class InterviewResultSerializer(serializers.ModelSerializer):
@@ -53,7 +52,7 @@ class QuestionListSerializer(serializers.ModelSerializer):
   def get_type_name_for_question(self, question):
     return question.question_type
 
-
+# 면접 답변 등록 Serializer
 class AnswerCreateSerializer(serializers.ModelSerializer):
   record_url = serializers.FileField()  # FileField를 사용하여 파일 객체를 받습니다.
 
@@ -61,21 +60,21 @@ class AnswerCreateSerializer(serializers.ModelSerializer):
     model = Answer
     fields = ['question', 'record_url']
 
-  def create(self, validated_data):
-    record_file = validated_data.pop('record_url')
+  # def create(self, validated_data):
+  #   record_file = validated_data.pop('record_url')
 
-    # AWS S3를 사용하는 경우
-    record_url = handle_uploaded_file_s3(record_file)
+  #   # AWS S3를 사용하는 경우
+  #   # record_url = handle_uploaded_file_s3(record_file)
 
-    # record_url을 다시 설정
-    validated_data['record_url'] = record_url
+  #   # record_url을 다시 설정
+  #   validated_data['record_url'] = record_url
 
-    return super().create(validated_data)
-  #Response에 변환된 url 보여주기
-  def to_representation(self, instance):
-    ret = super().to_representation(instance)
-    ret['record_url'] = instance.record_url  # 변환된 URL로 변경
-    return ret
+  #   return super().create(validated_data)
+  # #Response에 변환된 url 보여주기
+  # def to_representation(self, instance):
+  #   ret = super().to_representation(instance)
+  #   ret['record_url'] = instance.record_url  # 변환된 URL로 변경
+  #   return ret
   
 # 면접 목록 조회 Serializer
 class InterviewListSerializer(serializers.ModelSerializer):
