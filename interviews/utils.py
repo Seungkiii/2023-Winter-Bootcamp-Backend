c345a29b59ae5b0291d96fcba9bbacd5250214e7
Seungkiii <146107690+Seungkiii@users.noreply.github.com>
2024-01-15 02:16:03 +0900

    feat : 면접 생성시 질문생성 후 저장

--- interviews/utils.py
+++ interviews/utils.py
@@ -2,8 +2,10 @@ import boto3
 from uuid import uuid4
 from botocore.exceptions import NoCredentialsError
 import openai
+from openai import OpenAI
 
 from interviews.models import QuestionType, Question
+from resumes.models import Resume
 
 
 def handle_uploaded_file_s3(file):
@@ -23,26 +25,41 @@ def handle_uploaded_file_s3(file):
 
 
 
-openai.api_key = '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'
 
+client = OpenAI(api_key='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')
 
-def create_questions_withgpt(repo_name, type_name, position):
-    question_types = [QuestionType.PROJECT.value, QuestionType.CS.value, QuestionType.PERSONALITY.value]
+def create_questions_withgpt(repo_name, type_name, position, resume_id):
+    # Resume 테이블에서 resume_id에 해당하는 레코드를 가져옵니다.
+    resume = Resume.objects.get(id=resume_id)
+    # 이력서의 text_contents를 가져옵니다.
+    resume_contents = resume.text_contents
+    # GPT 함수에 resume_contents를 전달합니다.
+    question_types = [qt.value for qt in QuestionType if qt.value in type_name]
     questions = []
 
-    for question_type in question_types:
-        prompt = f"{repo_name}에 대한 {type_name}, {position} 관련 {question_type} 질문은 무엇인가요?"
 
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            temperature=0.5,
-            max_tokens=60
+
+    for question_type in question_types:
+        prompt = f"You are an interviewer at a company and are interviewing a developer. {resume_contents} is the contents of the interviewer's resume. Your task is to create only one question in Korean, not exceeding 200 characters, and {question_type} related to {type_name} and {position} based on the interviewer's resume. Any questions you make must be translated into Korean."
+
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            temperature=1,
+            messages=[
+                {
+                    "role": "system",
+                    "content": prompt
+                },
+
+            ]
         )
 
-        questions.append((response.choices[0].text.strip(), question_type))
+
+        questions.append((response.choices[0].message.content.strip(), question_type))
 
     return questions
+
+
 
 
 def save_question(questions, interview):
