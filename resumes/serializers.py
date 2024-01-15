1718d40072c6fc2ecc43dc5c67274ebc557a9f14
Seungkiii <146107690+Seungkiii@users.noreply.github.com>
2024-01-15 14:48:34 +0900

    feat : 이력서 id 출력 추가

--- resumes/serializers.py
+++ resumes/serializers.py
@@ -5,7 +5,7 @@ from .models import Resume
 class ResumeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resume
-        fields = ['user_id', 'pre_image_url','title','text_contents', 'created_at']
+        fields = ['id', 'user_id', 'pre_image_url','title','text_contents', 'created_at']
 
     def create(self, validated_data):
         # image_url이 validated_data에 포함되어 있음을 가정
