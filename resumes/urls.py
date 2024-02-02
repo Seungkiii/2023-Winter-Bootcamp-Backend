3270ae34e262c3f1600fd95b1c46e2da38a4a9c7
non <grapher285@gmail.com>
2024-02-02 09:57:43 +0900

    feat : api endpoint 수정

--- resumes/urls.py
+++ resumes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from .views import ResumeList, ResumeCreate, ResumeDelete
+from .views import ResumeDelete
 
 urlpatterns = [
-    path('', ResumeList.as_view(), name='resume-list'),
-    path('create', views.ResumeCreate.as_view(), name='resume-create'),
     path('delete/<int:id>', ResumeDelete.as_view(), name='resume-delete'),
 ]
